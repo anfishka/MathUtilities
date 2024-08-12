1f5f17caee2c907e5a704739d81e8a59d1d7c553
Hanna Borodina <venera131916@gmail.com>
2024-08-13 01:49:05 +0300

    Добавьте файлы проекта.

---
+++ Controllers/AverageValController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MathUtilities.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AverageValController : ControllerBase
+    {
+        [HttpGet]
+
+
+        public ActionResult<double> Get([FromQuery] List<int> data)
+        {
+            return data.Average();
+        }
+    
+}
+}
---
+++ Controllers/DateDiffController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MathUtilities.Controllers
+{
+   
+        [ApiController]
+        [Route("[controller]")]
+ public class DateDiffController : ControllerBase
+    {
+        [HttpGet]
+        public int Get(DateTime date)
+        {
+            DateTime today = DateTime.Today;
+            TimeSpan diff = today - date;
+            return Math.Abs(diff.Days) + 1;
+        }
+    }
+}
